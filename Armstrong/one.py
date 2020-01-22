431c174f39b4191af03dc01d25bebdbada15b8a1
Jackson Jacob <55303125+Jackson-hub@users.noreply.github.com>
2020-01-22 16:03:54 +0530

    Add files via upload
    Armstrong number using Modules.

---
+++ Armstrong/one.py
@@ -0,0 +1,3 @@
+def takeInput():
+    n=int(input("Enter a number to check if it is armstrong number: "))
+    return n
---
+++ Armstrong/three.py
@@ -0,0 +1,6 @@
+from one import takeInput 
+from two import armstrong
+if armstrong(takeInput()):
+    print("armstrong number")
+else:
+	print("not armstrong number")
